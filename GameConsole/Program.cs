e94c5bc610622968c90afd8265b2e1e446c7c4ac
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 11:49:01 +0100

    Null-conditional operator ?[

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 namespace GameConsole {
     class Program {
         static void Main(string[] args) {
-            PlayerCharacter player = null;
-            //player.DaysSinceLastLogin = 42;
+            PlayerCharacter[] players = new PlayerCharacter[3] {
+                new PlayerCharacter {Name="Sarah"},
+                new PlayerCharacter(), // Name = null
+                null // PlayerCharacter = null
+            };
 
-            int days = player?.DaysSinceLastLogin ?? -1;
+            // PlayerCharacter[] players = null;
 
+            string p1 = players?[0]?.Name;
+            string p2 = players?[1]?.Name;
+            string p3 = players?[2]?.Name;
             
-
-            Console.WriteLine(days);
         }
     }
 }
