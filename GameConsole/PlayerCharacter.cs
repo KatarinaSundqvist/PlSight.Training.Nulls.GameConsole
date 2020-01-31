7795b426c1350a1b2572c2e8ad547ffc249c123c
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 10:04:30 +0100

    Nullable types using the shorthand

--- GameConsole/PlayerCharacter.cs
+++ GameConsole/PlayerCharacter.cs
@@ -7,8 +7,8 @@ using System.Threading.Tasks;
 namespace GameConsole {
     class PlayerCharacter {
         public string Name { get; set; }
-        public Nullable<int> DaysSinceLastLogin { get; set; }
-        public Nullable<DateTime> DateOfBirth { get; set; }
+        public int? DaysSinceLastLogin { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
         public PlayerCharacter() {
             DateOfBirth = null;
