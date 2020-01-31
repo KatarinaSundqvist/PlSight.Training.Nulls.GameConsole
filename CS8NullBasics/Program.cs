37d4421286213b5ede09e4b10278caaed0a8d697
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 16:15:22 +0100

    Enabling Non-nullable references for the entire program

---
+++ CS8NullBasics/Program.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+
+namespace CS8NullBasics {
+    class Program {
+        static void Main(string[] args) {
+#nullable disable
+            string message = null;
+#nullable enable
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+
+        }
+    }
+}
