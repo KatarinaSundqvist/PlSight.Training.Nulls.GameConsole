ab80b8df3a8aa7ed01a1ecd8b21a68c01d622e26
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 11:28:42 +0100

    Null-coalescing operator

--- GameConsole/PlayerDisplayer.cs
+++ GameConsole/PlayerDisplayer.cs
@@ -10,7 +10,8 @@ namespace GameConsole {
                 Console.WriteLine(player.Name);
             }
 
-            int days = player.DaysSinceLastLogin.HasValue ? player.DaysSinceLastLogin.Value : -1;
+            int days = player.DaysSinceLastLogin ?? -1;
+            //int days = player.DaysSinceLastLogin.HasValue ? player.DaysSinceLastLogin.Value : -1;
             //int days = player.DaysSinceLastLogin.GetValueOrDefault(-1);
             Console.WriteLine($"{days} days since last login");
 
