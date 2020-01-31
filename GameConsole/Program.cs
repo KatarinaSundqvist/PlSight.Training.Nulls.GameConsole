0755086f53d8bb37d80002cb06d043112ae38f8f
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 10:02:21 +0100

    Refactored to use Nullable<T> (not the short-hand)

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -9,7 +9,7 @@ namespace GameConsole {
         static void Main(string[] args) {
             var player = new PlayerCharacter();
             player.Name = "Sarah";
-            player.DaysSinceLastLogin = 42;
+            //player.DaysSinceLastLogin = 42;
             PlayerDisplayer.Write(player);
         }
     }
