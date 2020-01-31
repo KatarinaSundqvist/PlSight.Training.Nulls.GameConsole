3aa27c0629cee5de265f386779fd84a1516dfe03
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 11:37:25 +0100

    Null-conditional operator together with null-coalescing operator

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -7,10 +7,14 @@ using System.Threading.Tasks;
 namespace GameConsole {
     class Program {
         static void Main(string[] args) {
-            var player = new PlayerCharacter();
-            player.Name = "";
+            PlayerCharacter player = null;
             //player.DaysSinceLastLogin = 42;
-            PlayerDisplayer.Write(player);
+
+            int days = player?.DaysSinceLastLogin ?? -1;
+
+            
+
+            Console.WriteLine(days);
         }
     }
 }
