3f534639e09b3ec80ad6f46df3e1f591527f0bea
KatarinaSundqvist <singsongkat@live.com>
2020-01-31 11:25:39 +0100

    Conditional operator

--- GameConsole/Program.cs
+++ GameConsole/Program.cs
@@ -8,7 +8,7 @@ namespace GameConsole {
     class Program {
         static void Main(string[] args) {
             var player = new PlayerCharacter();
-            player.Name = "Sarah";
+            player.Name = "";
             //player.DaysSinceLastLogin = 42;
             PlayerDisplayer.Write(player);
         }
